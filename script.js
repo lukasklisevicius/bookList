b06c879b9712b2744ef3cf9031cf834d32ab06c1
lukasklisevicius <8200Lukas@gmail.com>
2021-03-31 22:32:59 +0300

    padarytas stilius - jei reikes ka dar pataisysiu

---
+++ script.js
@@ -0,0 +1,30 @@
+// formos generavimo logika naudojant oop
+// apziureti butinas klaidas pagal pvz
+// zvaigdutes ir DOM DOM
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("edit");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
